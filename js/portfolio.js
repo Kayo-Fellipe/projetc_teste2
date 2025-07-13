c0464c99a0dca1884ec6c43cd1e89ee899e217a8
Kayo Fellipe <167824869+Kayo-Fellipe@users.noreply.github.com>
2025-07-12 23:33:21 -0300

    Atualizar para link público do Telegram

--- js/portfolio.js
+++ js/portfolio.js
@@ -314,7 +314,7 @@ const portfolioData = {
     items: [
       {
         type: "video",
-        src: "https://t.me/c/2628580973/2?embed=1",
+        src: "https://t.me/midiaupload/2?embed=1",
         thumbnail: "https://i.postimg.cc/dQ8Tj714/DSC01249-Original.jpg",
       },
     ]
